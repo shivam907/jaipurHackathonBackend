0a9cb0317ec18a08ac789d5b930329a395c91338
shivam907 <shivamkaushal907@gmail.com>
2023-03-21 07:31:54 +0530

    Deploying...

--- router/mainRoutes.js
+++ router/mainRoutes.js
@@ -12,9 +12,9 @@ router.post("/postUser", mainControllers.postUserData);
 router.post("/log", mainControllers.login);
 router.get("/tools", mainControllers.tools);
 // router.post("/tool", mainControllers.tool);
-router.post("/upload", upload.single("file"), async (req, res) => {
-  if (req.file === undefined) return res.send("you must select a file.");
-  const imgUrl = `http://localhost:3000/file/${req.file.filename}`;
-  return res.send(imgUrl);
-});
+// router.post("/upload", upload.single("file"), async (req, res) => {
+//   if (req.file === undefined) return res.send("you must select a file.");
+//   const imgUrl = `http://localhost:3000/file/${req.file.filename}`;
+//   return res.send(imgUrl);
+// });
 module.exports = router;
